b4fb096ef1f55572b8a28197bb43c89e67c2d0d4
Alexey Saulin <alexey.saulin03@mail.ru>
2022-05-05 00:29:18 +0300

    Solution LINQ

---
+++ ConsoleApp3/ConsoleApp3/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp3
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+            var entity = new List<Entity>();
+            for (int i = 0; i < 5; i++)
+            {
+                var ent = new Entity();
+                if (i == 0)
+                {
+                    ent.Id = i + 1;
+                    ent.ParentId = i;
+                    ent.Name = "Root entity";
+                }
+                else
+                {
+                    int k = rnd.Next(0, 4);
+                    ent.Id = i + 1;
+                    ent.ParentId = k;
+                    ent.Name = $"Child of {k} entity";
+                }
+                entity.Add(ent);
+            }
+
+            var result = entity
+                .GroupBy(s => s.ParentId);
+            foreach (var key in result)
+            {
+                Console.Write($"Key = {key.Key}, ");
+                Console.Write($"Value = List [ ");
+                foreach (var value in key)
+                {
+                    Console.Write($"Entity[Id = {value.Id}], ");
+                }
+
+                Console.Write(" ]");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
